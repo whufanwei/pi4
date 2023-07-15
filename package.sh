8cc5dccb1d1736d5d6237b8c6926991a205d21e5
whufanwei <whufanwei@gmail.com>
2023-07-15 12:24:52 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,8 +3,6 @@
 # vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
-git clone https://github.com/fw876/helloworld package/otherapp/helloworld
-git clone https://github.com/xiaorouji/openwrt-passwall package/otherapp/passwall
 
 # Theme
 # luci-theme-neobird

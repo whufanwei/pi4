731f0d5553c571867878395c8972adcf4ccf6114
whufanwei <whufanwei@gmail.com>
2023-07-15 12:24:02 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
-git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
-git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
+
 # vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
+git clone https://github.com/fw876/helloworld package/otherapp/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall package/otherapp/passwall
 
 # Theme
 # luci-theme-neobird

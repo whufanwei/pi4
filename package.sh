f310a8df90f469f118fca4f9667bbd8fa2f94303
whufanwei <whufanwei@gmail.com>
2023-07-16 10:32:08 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
 
-# vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
-
-# Theme
-# luci-theme-neobird
-git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 

d0315fdb2f5c73e9748e68e1665ee65bfe2ec048
whufanwei <whufanwei@gmail.com>
2024-03-14 23:02:54 +0800

    mosdns

--- configure.sh
+++ configure.sh
@@ -8,6 +8,8 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
+
+
 # luci-theme-infinityfreedom
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
@@ -18,6 +20,10 @@ echo 'src-git neobird https://github.com/thinktip/luci-theme-neobird.git' >>feed
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+
+# mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 修改默认IP为192.168.50.100
 sed -i 's/192.168.1.1/192.168.50.100/g' package/base-files/files/bin/config_generate 
